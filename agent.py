ff3505727e30472698c030a250eac67527eec0e9
Maryam <mariamwaqarawan@gmail.com>
2025-12-15 02:21:42 -0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -5,24 +5,30 @@ from livekit import agents, rtc
 from livekit.agents import AgentSession, Agent, JobContext, WorkerOptions
 from livekit.plugins import openai, silero
 
-# 1. Define the Agent's behavior (Instructions and optional tools)
+
 class VoiceAssistant(Agent):
     def __init__(self) -> None:
-        super().__init__(instructions="You are a helpful and polite voice AI assistant. Keep your responses concise and conversational.")
+        super().__init__(
+            instructions=(
+                "You are a helpful and polite receptionist at a dental clinic. "
+                "You're responsible for taking appointments from callers, checking availability "
+                "for appointment timings and doctors, and managing the entire database. "
+                "Keep your responses concise and conversational."
+            )
+        )
 
 # 2. Define the Entrypoint function to handle the job
 async def entrypoint(ctx: JobContext):
-    # Connect the agent to the room defined in the JobContext
+    # Connect the agent to the room defined in the JobContext 
     await ctx.connect()
 
     # Create an instance of your Agent class
     agent_instance = VoiceAssistant()
     
-    
     session = AgentSession(
-        stt=openai.STT(),
-        llm=openai.LLM(),
-        tts=openai.TTS(voice="alloy"),
+        stt=openai.STT(model="gpt-4o-transcribe"),
+        llm=openai.LLM(model="gpt-4o"),
+        tts=openai.TTS( model="gpt-4o-mini-tts",voice="alloy"),
         vad=silero.VAD.load(),
     )
 
@@ -35,7 +41,8 @@ async def entrypoint(ctx: JobContext):
 if __name__ == '__main__':
     # Use the entrypoint_fnc parameter in WorkerOptions
     opts = WorkerOptions(
-        entrypoint_fnc=entrypoint
+        entrypoint_fnc=entrypoint,
+        agent_name="dental_receptionist"
     )
     
     # Run the Agent Server
